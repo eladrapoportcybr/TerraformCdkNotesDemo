f4021a47cb14c6bd7050eaf6382f2d4cd60ab1f5
Elad Rapoport <elad.rapoport@cyberark.com>
2020-09-22 15:00:19 +0300

    EV-000: small refactor

--- notes_handler.py
+++ notes_handler.py
@@ -8,32 +8,21 @@ s3 = boto3.resource("s3")
 obj = s3.Object('eladr-terraform-cdk-demo-bucket', "notes.json")
 
 
+# GET
 def get_notes_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(read_notes())
-    }
+    return {'statusCode': 200, 'body': json.dumps(read_notes())}
 
 
+# POST
 def add_note_handler(event: dict, context):
-    try:
-        new_note = update_notes(json.loads(event["body"])['content'])
-    except Exception as err:
-        return {
-            'statusCode': 200,
-            'body': str(err)
-        }
-    return {
-        'statusCode': 201,
-        'body': json.dumps(new_note)
-    }
+    new_note = update_notes(json.loads(event["body"])['content'])
+    return {'statusCode': 201, 'body': json.dumps(new_note)}
 
 
+# DELETE
 def delete_note_handler(event, context):
     delete_note(json.loads(event["body"])['id'])
-    return {
-        'statusCode': 204
-    }
+    return {'statusCode': 204}
 
 
 def id_generator():
